fc338b2dc726ead6ad0e933d92df87be41e4aaf8
Omri Naor <omrin2011@gmail.com>
2022-12-09 11:22:27 +0200

    Upload Project

---
+++ hex/Constants.java
@@ -0,0 +1,23 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package hex;
+
+/**
+ *
+ * @author omri
+ */
+public interface Constants {
+	enum pos { PLUS , MINUS };
+    public static final int SIZE = 9;
+    public static final int WALL_SIZE = 4;
+    public static final int FULL_BOARD = 74;
+    public static final int WALL = -1;
+    public static final int EMPTY = 0;
+    public static final int PLAYER1 = 1;
+    public static final int PLAYER2 = 2;
+    public static final int IMG_FOR_PLAYER1 = 3;
+    public static final int IMG_FOR_PLAYER2 = 4;
+}
